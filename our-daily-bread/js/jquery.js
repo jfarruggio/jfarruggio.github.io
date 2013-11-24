86cb8beff47d2f07ee515452f66c5eea3a3290f7
jfarruggio <josephfarruggio@Josephs-MacBook-Pro.local>
2013-11-23 21:45:43 -0600

    improved jquery

--- our-daily-bread/js/jquery.js
+++ our-daily-bread/js/jquery.js
@@ -9,4 +9,15 @@ $(document).ready(function() {
         $('.read-more').fadeIn('fast');
         $('.hide-show-mobile').fadeOut('fast');
     });
+
+    $('.read-more2').click(function() {
+        $('.read-more2').fadeOut('fast');
+        $('.read-less2').fadeIn('fast');
+        $('.hide-show-mobile2').fadeIn('fast');
+    });
+    $('.read-less2').click(function() {
+        $('.read-less2').fadeOut('fast');
+        $('.read-more2').fadeIn('fast');
+        $('.hide-show-mobile2').fadeOut('fast');
+    });
 });
